973186953f357232ad48f762e38c40053b9b3ac9
Will Shahda <will.shahda@gmail.com>
2019-03-12 22:02:33 -0400

    Implement CTA scroll

--- client/src/Home.js
+++ client/src/Home.js
@@ -5,19 +5,34 @@ import Layout from './Layout';
 import PageHeader from './PageHeader';
 import FindWhales from './FindWhales';
 
+const onCallToAction = formRef => {
+  return () => {
+    formRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  };
+};
+
+
 export default withWeb3()(props => {
   const { web3 } = props;
+
+  const formRef = React.createRef();
 
   return (
     <Layout web3={web3}>
       <PageHeader
         title="WhaleHunter"
         subtitle="Find whales for ERC-20 tokens"
-        callToAction="FIND WHALES"
+        callToAction="HUNT FOR WHALES"
+        onCallToAction={onCallToAction(formRef)}
         fullscreen
       />
       <Container text>
-        <FindWhales web3={web3} />
+        <div ref={formRef}>
+          <FindWhales web3={web3} />
+        </div>
       </Container>
     </Layout>
   );
